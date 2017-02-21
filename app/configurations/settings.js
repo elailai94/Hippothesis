09725e3b63a9e9934e4b3cb1a38cb74e8dc33bb5
Elisha Lai <elisha.lai@outlook.com>
2017-02-20 20:32:04 -0500

    Add API base url and key

--- app/configurations/settings.js
+++ app/configurations/settings.js
@@ -1,2 +1,8 @@
+/* @flow */
+
 'use strict';
 
+export const settings = {
+	API_BASE_URL: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/',
+   API_KEY: 'jVDiaXvfpsmshdrtFO7JHibo7Hrtp16UelPjsnBE4Bfzl0CV5u'
+};
