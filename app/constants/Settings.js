fded2656bd3b563536a7878b7d68f2510f726963
Elisha Lai <elisha.lai@outlook.com>
2017-03-01 11:32:23 -0500

    Add new endpoint to get random recipes

--- app/constants/Settings.js
+++ app/constants/Settings.js
@@ -17,6 +17,7 @@ const Settings = {
     AUTOCOMPLETE_INGREDIENT_SEARCH_PATH: '/food/ingredients/autocomplete',
     AUTOCOMPLETE_RECIPE_SEARCH_PATH: '/recipes/autocomplete',
     COMPLEX_RECIPE_SEARCH_PATH: '/recipes/searchComplex',
+    GET_RANDOM_RECIPES_SEARCH_PATH: '/recipes/random',
     GET_RECIPE_INFORMATION_PATH: '/recipes/{id}/information',
     FIND_SIMILAR_RECIPES_PATH: '/recipes/{id}/similar',
     GENERATE_MEAL_PLAN_PATH: '/recipes/mealplans/generate'
