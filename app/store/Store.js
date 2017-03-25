f8e11f95e3637040e41389ede9434fc8c1e2c35f
yfjd01 <yfjd01@gmail.com>
2017-03-25 13:15:00 -0400

    persist

--- app/store/Store.js
+++ app/store/Store.js
@@ -9,20 +9,44 @@
 'use strict';
 
 import {
+  compose,
   createStore,
   applyMiddleware
 } from 'redux';
+import {
+  persistStore, 
+  autoRehydrate
+} from 'redux-persist';
+import {
+  AsyncStorage
+} from 'react-native';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-
 import RootReducer from '../reducers/RootReducer';
 
 const Store = null;
 if (process.env.NODE_ENV === 'development') {
   const logger = createLogger();
-  Store = createStore(RootReducer, applyMiddleware(thunk, logger));
+  Store = createStore(
+    RootReducer, 
+    undefined,
+    compose(
+      applyMiddleware(thunk, logger),
+      autoRehydrate() 
+    )
+  );
 } else {
-  Store = createStore(RootReducer, applyMiddleware(thunk));
+  Store = createStore(
+    RootReducer, 
+    undefined, 
+    compose(
+      applyMiddleware(thunk), 
+      autoRehydrate()
+    )
+  );
 }
+
+// begin periodically persisting the store
+persistStore(Store, {storage: AsyncStorage});
 
 export default Store;
